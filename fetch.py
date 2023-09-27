0b923fcba7ce33ba3e2859caf2a026376c564b1e
Jorge Gomes <jorge.gomes@vost.pt>
2023-09-27 20:35:14 +0100

    Update fetch.py
    This update fixes the run error of the GitHub Actions, where "Aviso de Sismo" is not in the Title

--- fetch.py
+++ fetch.py
@@ -51,6 +51,9 @@ if __name__ == "__main__":
     url = "https://www.ipma.pt/resources.www/rss/comunicados.xml"
     df = fetch_xml_data(url)
 
+    # Filter the DataFrame to only include rows with "Aviso de Sismo" in the Title column
+    df = df[df["Title"].str.contains("Aviso de Sismo", case=False, na=False)]
+
     # Extract date_time from the Description column
     df['date_time'] = df['Description'].apply(lambda x: re.search(r'(\d{2}-\d{2}-\d{4} pelas \d{2}:\d{2} \(hora local\))', x).group(1) if re.search(r'(\d{2}-\d{2}-\d{4} pelas \d{2}:\d{2} \(hora local\))', x) else None)
 
