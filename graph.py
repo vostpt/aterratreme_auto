216f622cf0b45a5e96b9a78d3650fecbd2f98d05
pedrolucas7i <145859069+pedrolucas7i@users.noreply.github.com>
2024-07-18 12:53:15 +0100

    All PCs were given permission to access the application and update the requirements

--- graph.py
+++ graph.py
@@ -185,4 +185,4 @@ def update_description_data(selected_date):
         return html.Div("No data available")
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(host='0.0.0.0', port=8050, debug=False)
